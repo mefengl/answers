0e619367f10baa6284a50f6683322510384f5cf6
mefengl <71683364+mefengl@users.noreply.github.com>
2022-08-26 16:56:40 +0800

    3

---
+++ 319.灯泡开关.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=319 lang=python3
+#
+# [319] 灯泡开关
+#
+
+# @lc code=start
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(math.sqrt(n))
+
+
+# @lc code=end
