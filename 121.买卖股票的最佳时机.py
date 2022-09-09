157d693406c686782bbcc6221ef15ff2c81c7417
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-09-09 19:02:56 +0800

    1 4

--- 121.买卖股票的最佳时机.py
+++ 121.买卖股票的最佳时机.py
@@ -7,15 +7,15 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices) + 1
+        n = len(prices)
         dp = [[0] * 2 for _ in range(n)]
         # 第一天就买入
         dp[0][1] = -prices[0]
         for i in range(1, n):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1])
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
             # 只能买一次
             # dp[i][1] = max(dp[i-1][1],dp[i-1][0]-prices[i-1])
-            dp[i][1] = max(dp[i - 1][1], -prices[i - 1])
+            dp[i][1] = max(dp[i - 1][1], -prices[i])
         return dp[-1][0]
 
 
--- 122.买卖股票的最佳时机-ii.py
+++ 122.买卖股票的最佳时机-ii.py
@@ -7,12 +7,12 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices) + 1
+        n = len(prices)
         dp = [[0] * 2 for _ in range(n)]
         dp[0][1] = -prices[0]
         for i in range(1, n):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1])
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1])
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
         return dp[-1][0]
 
 
