5aef9c922d12f711da7633f6a4340fb54511ca0f
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-09-14 02:15:09 +0800

    0 0

--- 46.全排列_old.py
+++ 46.全排列_old.py
@@ -5,29 +5,40 @@
 #
 
 # @lc code=start
+def dfs(nums: List[int], path: List[int], res: List[List[int]]) -> None:
+    if len(nums) == 0:
+        res.append(path)
+    for i in range(len(nums)):
+        dfs(nums[:i] + nums[i + 1 :], path + [nums[i]], res)
 
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        long = len(nums)
-        used = [False] * long
-        path = []
-        result = []
+        res = []
+        dfs(nums, [], res)
+        return res
 
-        def backtrack():
-            if len(path) == long:
-                result.append(path.copy())
-            for idx in range(long):
-                if used[idx] == True:
-                    continue
-                used[idx] = True
-                path.append(nums[idx])
-                backtrack()
-                path.pop()
-                used[idx] = False
+# # 回溯法
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         long = len(nums)
+#         used = [False] * long
+#         path = []
+#         result = []
 
-        backtrack()
-        return result
+#         def backtrack():
+#             if len(path) == long:
+#                 result.append(path.copy())
+#             for idx in range(long):
+#                 if used[idx] == True:
+#                     continue
+#                 used[idx] = True
+#                 path.append(nums[idx])
+#                 backtrack()
+#                 path.pop()
+#                 used[idx] = False
 
+#         backtrack()
+#         return result
 
 # @lc code=end
