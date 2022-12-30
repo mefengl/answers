1b5eb66e137d5fe58c5eafe0e77bcb4db96b79c2
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-12-30 14:16:21 +0800

    lodash-own-deepMerge: update, add undefined check

--- lodash-to-me/own-lodash/deep-merge.js
+++ lodash-to-me/own-lodash/deep-merge.js
@@ -1,11 +1,12 @@
 const deepMerge = (obj1, obj2) => {
   const result = { ...obj1 };
   Object.keys(obj2).forEach(key => {
-    result[key] = obj2[key] instanceof Object
-      ? Array.isArray(obj2[key])
-        ? [...obj1[key], ...obj2[key]]
-        : deepMerge(obj1[key], obj2[key])
-      : obj2[key];
+    result[key] =
+      (obj1[key] && obj1[key] instanceof Object)
+        ? Array.isArray(obj2[key])
+          ? [...obj1[key], ...obj2[key]]
+          : deepMerge(obj1[key], obj2[key])
+        : obj2[key];
   });
   return result;
 };
