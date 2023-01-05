4f67723edf33fce1d708b641ae4ab469f7ad6b0b
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-01-05 11:22:14 +0800

    own-lodash: update deep-merge
    remove array merge, fix the issue caused by `array is also a object`

--- lodash-to-me/own-lodash/deep-merge.js
+++ lodash-to-me/own-lodash/deep-merge.js
@@ -2,10 +2,8 @@ const deepMerge = (obj1, obj2) => {
   const result = { ...obj1 };
   Object.keys(obj2).forEach(key => {
     result[key] =
-      (obj1[key] && obj1[key] instanceof Object)
-        ? Array.isArray(obj2[key])
-          ? [...obj1[key], ...obj2[key]]
-          : deepMerge(obj1[key], obj2[key])
+      (obj1[key] && obj1[key] instanceof Object && !(obj1[key] instanceof Array))
+        ? deepMerge(obj1[key], obj2[key])
         : obj2[key];
   });
   return result;
